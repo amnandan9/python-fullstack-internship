8d4a927df441859ae3ffcd954504d86fb46a6873
Nandan A M <95686472+amnandan9@users.noreply.github.com>
2026-02-05 08:22:32 +0530

    practiced swapping numbers using python interactive example

---
+++ 03-02-2026.py
@@ -0,0 +1,7 @@
+'''
+These are the exercise for Swaping of variables
+'''
+
+a=1, b=2, c=3, d=4, e=a, f=d, b=d, d=f, f=d, a=b, b=e
+h=10, i=20, j=30, k=40, l=h, m=j, j=k, k=i, p=k
+a=10, b=20, c=a, a=b, b=c
