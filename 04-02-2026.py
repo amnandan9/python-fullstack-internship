0f5b356ecf7811583ea317205dcd8716f7a905ac
Nandan A M <95686472+amnandan9@users.noreply.github.com>
2026-02-10 05:56:09 +0530

    practiced examples of SVDT

---
+++ 04-02-2026.py
@@ -0,0 +1,10 @@
+'''
+Single Value Data Type
+int, float, complex, bool
+'''
+a=89, b=409, c=34 '''int'''
+a=89.0, b=43.43, c=43.234 '''float'''
+a=8j, b=9j+56, c=34+0j '''complex'''
+
+'''in complex value the real and imaginary part can be inter changed but the coefficient of imaginary part cannot
+be interchanged'''
