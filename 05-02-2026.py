3398d41f99ed19e02e187ec0fe7b00d0bd88c9f7
Nandan A M <95686472+amnandan9@users.noreply.github.com>
2026-02-10 06:36:31 +0530

    practice example on string and indexing

---
+++ 05-02-2026.py
@@ -0,0 +1,56 @@
+'''string and indexing'''
+
+Example 1
+x="manga mangi dinga dingi"
+1st i
+2nd n
+a
+3rd a
+m
+ans:
+>>> x="manga mangi dinga dingi"
+>>> x[10]
+'i'
+>>> x[-13]
+'i'
+>>> x[-21]
+'n'
+>>> x[2]
+'n'
+>>> x[-22]
+'a'
+>>> x[1]
+'a'
+>>> x[7]
+'a'
+>>> x[-16]
+'a'
+>>> x[0]
+'m'
+>>> x[-23]
+'m'
+
+
+Example 2
+s = "python"
+Find:
+1st → ?
+2nd → ?
+4th → ?
+Last → ?
+
+Example 3
+a = "computer"
+Find:
+1st letter
+5th letter
+8th letter
+2nd last letter
+
+Q4 (Harder)
+s = "python programming"
+Find:
+First character
+Character at index 7
+Character at index -5
+Space index
