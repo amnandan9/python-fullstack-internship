43eb0d72ed7c88d95b3db975bb03342bbc112fd4
Nandan A M <95686472+amnandan9@users.noreply.github.com>
2026-02-18 11:30:47 +0530

    practice example on operators

---
+++ 12-02-2026.py
@@ -0,0 +1,19 @@
+''' Operator in Python '''
+''' 1. Arithmetic Operator
+    2. Logical Operator
+    3. Membership Operator
+    4. Identity Operator
+    5. Assignment Operator
+    6. Relation Operator/Comparision Operator 
+
+1. Arithmetic Operator
+   a. addition operator
+   b. subtraction operator
+   c. multiplication operator
+   d. power operator
+   e. division operator
+       i. True Division
+       ii. floor Division
+       iii. Modulus Division Operator'''
+
+''' a. addition Operator '''
