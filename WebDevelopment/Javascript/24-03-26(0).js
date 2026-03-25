36716c8a84815307b3f27023526ddaf9ab00fff7
Nandan A M <95686472+amnandan9@users.noreply.github.com>
2026-03-25 11:49:35 +0530

    DOM element creation and API fetch with dynamic user rendering from notes

---
+++ WebDevelopment/Javascript/24-03-26(0).js
@@ -0,0 +1,102 @@
+// // let h2=document.createElement('h2')
+// // console.log(h2)
+// // document.body.appendChild(h2)
+// // console.log(h2)
+// // h2.innerText="Good Morning All"
+// // let h3=document.createElement('h3')
+// // console.log(h3)
+// // document.body.appendChild(h3)
+// // h3.innerText="ljkhdfsdsfghukio;ijhgf"
+// // let section=document.createElement('section')
+// // console.log(section)
+// // document.body.appendChild(section)
+// // let h2=document.createElement('h2')
+// // console.log(h2)
+// // h2.innerText="Prajwal Chandrashekar"
+// // let img=document.createElement('img')
+// // console.log(img)
+// // img.src="https://avatars.githubusercontent.com/u/160094074?v=4"
+// // section.append(h2,img)
+// // let section=document.createElement('section')
+// // console.log(section)
+// // document.body.appendChild(section)
+// // section
+// // <table border="2px">
+// // <tr>
+// // <td>1</td>
+// // <td>2</td>
+// // </tr>
+// // <tr>
+// // <td>3</td>
+// // <td>4</td>
+// // </tr>
+// // </table>
+// // async function fetchData(){
+// // let data=await fetch('https://api.escuelajs.co/api/v1/users');
+// // console.log(data)
+// // let userData=await data.json()
+// // console.log(userData)
+// // let section=document.createElement('section')
+// // document.body.appendChild(section)
+// // userData.map((user)=>{
+//     // let aside=document.createElement('aside')
+// // section.append(aside)
+// // let h3=document.createElement('h3')
+// // let img=document.createElement('img')
+// // let p=document.createElement('p');
+// // p.innerText=`${user.password}`
+// // h3.innerText=`${user.name}`
+// // img.scr=`${user.avatar}`
+// // aside.append(h3,img,p)
+// // console.log(user)
+// // })
+// // }
+// // fetchData()
+
+// fetch('https://api.escuelajs.co/api/v1/users')
+// .then((res)=>{
+//  console.log(res);
+//  let userDetails=res.json()
+//  console.log(userDetails);
+ 
+//  userDetails.then((res)=>{
+//  console.log(res);
+//  let section=document.createElement('section')
+//  document.body.appendChild(section)
+//  res.map((user)=>{
+//  console.log(user);
+//  let aside=document.createElement('aside')
+//  section.appendChild(aside)
+//  let h2=document.createElement('h2')
+//  let img=document.createElement('img')
+//  h2.innerText=`${user.name}`
+//  img.src=`${user.avatar}`
+//  aside.append(h2,img)
+ 
+//  })
+ 
+//  })
+ 
+ 
+// })
+// // async function fetchData() {
+// // let data = await fetch('https://api.escuelajs.co/api/v1/users');
+// // console.log(data);
+// // let usersData = await data.json();
+// // console.log(usersData);
+// // let section = document.createElement('section');
+// // document.body.appendChild(section);
+// // usersData.map(user => {
+// // let aside = document.createElement('aside');
+// // section.append(aside);
+// // let h3 = document.createElement('h3');
+// // let img = document.createElement('img');
+// // let p = document.createElement('p');
+// // p.innerText = ${user.password};
+// // h3.innerText = ${user.name};
+// // img.src = ${user.avatar};
+// // aside.append(h3, img, p)
+// // console.log(user)
+// // });
+// // }
+// // fetchData()
